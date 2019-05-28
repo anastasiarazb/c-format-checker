268299da050b1f8da9f316f66168320b4d30223d
nastusha-merry <nastusha-merry@mail.ru>
2019-05-28 14:11:38 +0300

    add arguments parser (https://github.com/jarro2783/cxxopts)

--- src/common.h
+++ src/common.h
@@ -1,11 +1,29 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define DEBUG_LEXER
-#ifdef  DEBUG_LEXER
-#define log_l printf
-#else
-#define log_l(...) {}
-#endif
+//#define DEBUG_LEXER
+//#ifdef  DEBUG_LEXER
+//#define log_l printf
+//#else
+//#define log_l(...) {}
+//#endif
+
+//enum {
+//    EXIT_CODE_OK = 0,
+//    EXIT_CODE_INDENT_ERROR = 1,
+//    EXIT_CODE_MIXED_SPACES = 2,
+//    EXIT_CODE_INVALID_ARG_ERROR = 3,
+//    EXIT_CODE_FILE_ACCESS_ERROR = 4,
+//    EXIT_CODE_PARSING_ERROR = 5,
+//    EXIT_CODE_INTERNAL_ERROR = 6
+//};
+
+#define EXIT_CODE_OK 0
+#define EXIT_CODE_INDENT_ERROR 1
+#define EXIT_CODE_MIXED_SPACES 2
+#define EXIT_CODE_INVALID_ARG_ERROR 3
+#define EXIT_CODE_FILE_ACCESS_ERROR 4
+#define EXIT_CODE_PARSING_ERROR 5
+#define EXIT_CODE_INTERNAL_ERROR 6
 
 #endif //LEXER_H
--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,97 @@
 #include "ast/analyzer.hpp"
 #include "ast/line.hpp"
 #include "../include/nlohmann/json.hpp"
+#include "../include/cxxopts.hpp"
+#include "common.h"
 
 using json = nlohmann::json;
 
-void test_params(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cout << "Too few arguments!\n Usages: <program_source_file>\n. Please, try again!"
-                  << std::endl;
-        exit(2);
-    }
+struct Params {
+    int argc;
+    enum class LogLevel {QUIET, VERBOSE, DEBUG, UNINITIALIZED} log_level = LogLevel::QUIET;
+    std::string source;
+    std::string logfile = "c-format-checker.log";
+//    const std::string usage = "Usage: ./c-format-checker FILE [-q | -v | -d] [--logfile=FILE].\n"
+//                              "Use «./c-format-checker --help» for explanation.";
+    // Message similarly to the message for "grep" in GNU/Linux console
+    const std::string usage = "Использование: ./c-format-checker [-f] ФАЙЛ [-q | -v | -d] [-l ФАЙЛ].\n"
+                              "Запустите «./c-format-checker --help» для более подробного описания.\n";
+    const std::string man =  // Аналогично русскояз. мануалу https://www.opennet.ru/man.shtml?topic=grep&category=1
+        "НАЗВАНИЕ\n"
+        "       c-format-checker - проверка файла исходных текстов программ на языке C  на корректность расстановки отступов.\n"
+        "СИНТАКСИС\n"
+        "       ./c-format-checker [-f] ФАЙЛ [-q | -v | -d] [-l ФАЙЛ]\n"
+        "ОПИСАНИЕ\n"
+        "       Программа сканирует файл и проверяет корректность расстановки отступов.\n"
+        "       Если ошибки не обнаружены, программа завершается с кодом "
+        + std::to_string(EXIT_CODE_OK) + ".\n"
+        "       Если на одинаковом уровне вложенности строки имеют разный отступ, выдается сообщение об ошибке, "
+        "программа завершается с кодом " + std::to_string(EXIT_CODE_INDENT_ERROR) +
+        ".\n"
+        "       Если в файле часть строк начинается с табуляций, а часть - с пробелов, выдается сообщение об ошибке, "
+        "программа завершается с кодом " + std::to_string(EXIT_CODE_MIXED_SPACES) +
+        ". (Отступы во всем файле должны отбиваться одинаковыми символами, "
+        "комбинации пробелов и табуляций не допускаются.)\n"
+        "       Если программе некорректно переданы аргументы при вызове, программа завершается с кодом "
+        + std::to_string(EXIT_CODE_INVALID_ARG_ERROR) +
+        ".\n"
+        "       Если в программе произошла ошибка при попытке чтения/записи в файл, программа завершается с кодом "
+        + std::to_string(EXIT_CODE_FILE_ACCESS_ERROR) +
+        ".\n"
+        "       Если произошла ошибка на этапе синтаксического разбора (несоответствие переданного файла грамматике), "
+        "программа завершается с кодом " + std::to_string(EXIT_CODE_FILE_ACCESS_ERROR) +
+        ".\n"
+        "       Если во время выполнения программы произошла другая ошибка, программа завершается с кодом "
+        + std::to_string(EXIT_CODE_INTERNAL_ERROR) +
+        ".\n"
+        ;
 
-    if (argc > 2) {
-        std::cout << "Too many arguments, parameters ";
-        for (int i = 2; i < argc - 1; ++i) {
-            std::cout << "\"" << argv[i] << "\", ";
+
+    Params(int argc, char *argv[]) :
+    argc(argc)
+    {
+        cxxopts::Options options("c-format-checker",
+                                 "проверка файла исходных текстов программ на языке C на корректность расстановки отступов");
+        options.positional_help("[-f] ФАЙЛ [-q | -v | -d] [-l ФАЙЛ]");
+        options
+            .allow_unrecognised_options()
+            .add_options()
+                ("f,file",    "Файл исходного текста программы на языке c", cxxopts::value<std::string>())
+                ("q,quiet",   "Выводить кратко описание ошибок (по умолчанию)")
+                ("v,verbose", "Выводить развернутые описания ошибок", cxxopts::value<bool>()->default_value("false"))
+                ("d,debug",   "Выводить отладочную информацию", cxxopts::value<bool>()->default_value("false"))
+                ("help",      "Помощь", cxxopts::value<bool>()->default_value("false"))
+                ("l,logfile", "Имя файла для записи отладочной информации", cxxopts::value<std::string>())
+            ;
+        options.parse_positional("file");
+        auto result = options.parse(argc, argv);
+        if (result.count("help")) {
+            std::cout << man << std::endl;
+            exit(EXIT_CODE_OK);
         }
-        std::cout << "\"" << argv[argc - 1] << "\" ignored" << std::endl;
+        if (!result.count("file")) {
+            std::cout << usage << std::endl;
+            exit(EXIT_CODE_INVALID_ARG_ERROR);
+        }
+        source = result["file"].as<std::string>();
+        if (result.count("quiet") + result.count("verbose") + result.count("debug") > 1) {
+            std::cout << usage << std::endl;
+            exit(EXIT_CODE_INVALID_ARG_ERROR);
+        }
+        if (result.count("quiet"))
+            log_level = LogLevel::QUIET;
+        if (result.count("verbose"))
+            log_level = LogLevel::VERBOSE;
+        if (result.count("debug"))
+            log_level = LogLevel::DEBUG;
+        if (result.count("logfile")) {
+            logfile = result["logfile"].as<std::string>();
+        } else {
+            std::cout << usage << "\nФайл для вывода лога не указан, "
+                                  "использован файл по умолчанию «" << logfile << "»." << std::endl;
+        }
     }
-
-}
+};
 
 json read_rules(const std::string &path)
 {
@@ -51,12 +123,13 @@ json read_rules(const std::string &path)
     return j3;
 }
 
+
 int main(int argc, char* argv[])
 {
 
     Rules r("../rules.json");
     std::cout << "Rules:\n" << r << std::endl;
-    test_params(argc, argv);
+    Params params(argc, argv);
     Scanner scan0 (argv[1]);
     Token token;
     Line st(Indent(Token(lexem::Type::NEWLINE, "\n", Coords(), Coords())));
@@ -79,7 +152,7 @@ int main(int argc, char* argv[])
     Analyzer a(parser.get_token_table(), r);
     std::string mistakes = a.error_messages();
     if (mistakes.empty()) {
-        return 0;
+        return EXIT_CODE_OK;
     } else {
         return 1;
     }
