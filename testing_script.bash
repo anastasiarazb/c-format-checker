ff1bd34c09b7f7cb617375c6d93a1b173e38e027
nastusha-merry <nastusha-merry@mail.ru>
2019-05-31 07:09:39 +0300

    minor changes in scripts

--- testing_script.bash
+++ testing_script.bash
@@ -1,11 +1,18 @@
-prog="bin/c-format-checker"  # $1
+prog="c-format-checker/bin/c-format-checker"  # $1
 src="091 Фибоначчиевы строки"  # $2
+src="094 Наибольший простой делитель"
+#src="148 Полином"
 
 for i in "$src"/*
     do y=`eval "$prog" "\"$i\"" --quiet  --logfile="\"log/$(basename "$i").log\""`
-    if [ "$?" -eq 1 ]  # если программа завершилась с сообщением об ошибке отступа
+    res="$?"
+    if [ $res -eq 1 ]  # если программа завершилась с сообщением об ошибке отступа
     then 
-        echo "\"$i\""        # вывести имя файла
+        echo "$i"        # вывести имя файла
+    fi
+    if [ $res -eq 5 ]  # если программа завершилась с сообщением об ошибке на этапе парсинга
+    then 
+        echo "$i" >> parse_errors.log       # вывести имя файла в parse_errors.log
     fi
     # напечатать вывод программы в лог-файл
     echo "$y" > "out/$(basename "$i").log"
